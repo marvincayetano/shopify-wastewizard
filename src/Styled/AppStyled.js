9653378064f849f40a5fceedae61778598b6f942
Marvin Cayetano <marvin@marvin.local>
2019-01-17 23:32:31 -0500

    Remove button border

--- src/Styled/AppStyled.js
+++ src/Styled/AppStyled.js
@@ -55,6 +55,7 @@ const SearchDiv = styled.div`
   }
   >button {
     background-color: ${color.GREEN};
+    border: none;
     border-radius: 2px;
     box-shadow: 0 2px 2px 0 rgba(169, 169, 169, .14), 0 3px 1px -2px rgba(169, 169, 169, .2), 0 1px 5px 0 rgba(169, 169, 169, .12);
   }
